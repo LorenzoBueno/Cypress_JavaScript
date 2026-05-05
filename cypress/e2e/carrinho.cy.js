73a95b7fe3941973b297626f979a7e9c54aa696b
Lorenzo Bueno <lorenzo.s.m.bueno@gmail.com>
2026-05-05 10:30:39 -0300

    add gitignore

---
+++ cypress/e2e/carrinho.cy.js
@@ -0,0 +1,25 @@
+describe('Carrinho', () => {
+    it('Adicionar produto ao carrinho com sucesso', () => {
+        // arrange
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
+        //act
+        
+        cy.get('[data-test="add-to-cart-sauce-labs-backpack"]').click()
+
+
+        //assert
+        cy.get('[data-test="shopping-cart-badge"]')
+            .should(
+                "contain.text",
+                '1'
+            )
+            .and('be.visible') 
+
+        cy.get('[data-test="shopping-cart-link"]').click()
+        cy.contains('Sauce Labs Backpack').should('be.visible')
+    })
+})
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,39 @@
+describe('Login', () => {
+    it('Realizar login com sucesso', () => {
+
+        // arrange
+
+        cy.visit('https://www.saucedemo.com/')
+
+        // act
+
+        cy.get('[data-test="username"').type('standard_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
+        // assert
+
+        cy.url().should('eq', 'https://www.saucedemo.com/inventory.html')
+
+
+    })
+
+    it('Realizar login com fracasso', () => {
+        // arrange
+        cy.visit('https://www.saucedemo.com/')
+
+        // act
+        cy.get('[data-test="username"').type('invalido')
+        cy.get('[data-test="password"]').type('senha_invalida')
+        cy.get('[data-test="login-button"]').click()
+
+        //assert
+
+        cy.url().should('eq', 'https://www.saucedemo.com/')
+        cy.get('[data-test="error"]')
+            .should(
+                'contain.text',
+                'Epic sadface: Username and password do not match any user in this service'
+            )
+    })
+})
