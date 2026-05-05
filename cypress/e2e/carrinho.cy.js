caf47223e0b6a9a599eeb37dc1370163aadc125f
Lorenzo Bueno <lorenzo.s.m.bueno@gmail.com>
2026-05-05 11:20:52 -0300

    adding Add product failure and downloading report library

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -43,4 +43,23 @@ describe('Carrinho', () => {
         cy.get('[data-test="shopping-cart-badge"]').should('not.exist')
 
     })
+
+    it('Add product (Failure)', () => {
+
+        // arrange
+        cy.visit('https://www.saucedemo.com/')
+        cy.get('[data-test="username"').type('problem_user')
+        cy.get('[data-test="password"]').type('secret_sauce')
+        cy.get('[data-test="login-button"]').click()
+
+
+        //act
+
+        cy.get('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+
+        //assert
+
+        cy.get('[data-test="shopping-cart-badge"]').should('not.exist')
+
+    })
 })
