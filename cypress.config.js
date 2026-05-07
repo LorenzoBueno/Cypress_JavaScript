1bbb9984140eca2499f46801b55841a0e2da4bfd
Lorenzo Bueno <lorenzo.s.m.bueno@gmail.com>
2026-05-07 14:00:20 -0300

    Feature: ambient testing and scripts

--- cypress.config.js
+++ cypress.config.js
@@ -20,6 +20,7 @@ module.exports = defineConfig({
   trashAssetsBeforeRuns: true,
 
   e2e: {
+    baseUrl : 'https://www.saucedemo.com/',
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on)
       // implement node event listeners here
